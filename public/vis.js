3e6c632d614f340551b449963276feff1d640d69
Ben Karl <benrkarl@gmail.com>
2014-07-03 10:52:08 -0400

    set up size and scales for window SVG

--- public/vis.js
+++ public/vis.js
@@ -3,13 +3,44 @@ function buildSvg(data){
       width = 1500 - margin.right - margin.left,
       height = 1000 - margin.top - margin.bottom;
 
-  var newestPlanet = d3.max(data, function(planet){
-    return planet.disc_year
+  var newestPlanet = d3.max(data, function(year){
+    return year
   });
 
-  var oldestPlanet = d3.min(data, function(planet){
-    return planet.disc_year
+  var oldestPlanet = d3.min(data, function(year){
+    return year
   });
+
+  window.xAxisScale = d3.scale.linear().domain([oldestPlanet, newestPlanet]).range([width, 0]);
+  window.yAxisScale = d3.scale.linear().domain([0, 150]).range([height, 0]);
+
+  window.svg = d3.select('body')
+      .append('svg')
+        .attr('width', width + margin.right + margin.left)
+        .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+        .attr('height', height)
+        .attr('width', width)
+        .attr('class', 'timeline')
+        .attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+
+  var xAxis = d3.svg.axis()
+      .scale(window.xAxisScale)
+      .orient('bottom');
+
+  var yAxis = d3.svg.axis()
+      .scale(window.yAxisScale)
+      .orient('bottom');
+
+  window.svg.append('g')
+      .attr('class', 'x-axis')
+      .attr('transform', 'translate(0,' + height + ')' )
+      .call(xAxis);
+
+  window.svg.append('g')
+      .attr('class', 'y-axis')
+      .call(yAxis);
+
 }
 
 function getDiscYears(array){
@@ -31,7 +62,7 @@ $(function(){
     success: function(data){
       $('body').empty();
       window.yearArray = getDiscYears(data.response.results);
-      // buildSvg(planetData);
+      // buildSvg(yearArray);
     }
   })
 })
